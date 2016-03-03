69cf0751833481b69773475b02087f969f673584
NEHA SUWAl <neha@jyaasa.com>
2016-03-03 20:32:48 +0545

    Initial

---
+++ lib/my_first_fake_gem.rb
@@ -0,0 +1,8 @@
+require "my_first_fake_gem/version"
+
+module MyFirstFakeGem
+  # Your code goes here...
+  def self.hi
+    puts "Hello all!"
+  end
+end
---
+++ lib/my_first_fake_gem/version.rb
@@ -0,0 +1,3 @@
+module MyFirstFakeGem
+  VERSION = "0.1.0"
+end
